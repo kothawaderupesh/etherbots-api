b52d8ec3a4aa121e90bab5ea6a71c2ea11db9995
Priyanka-243 <priyanka.kothawade24@gmail.com>
2021-02-26 00:20:16 +0530

    Etherbots API - initial version

---
+++ src/test/java/com/toggle/EtherbotsTests.java
@@ -0,0 +1,14 @@
+package com.toggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EtherbotsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
